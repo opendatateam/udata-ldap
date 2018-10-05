a570ba1d23ada235d4e4014d9822dbe431617ecf
Axel Haustant <noirbizarre@gmail.com>
2018-10-05 18:21:13 +0200

    Bump 2.0

--- udata_ldap/__init__.py
+++ udata_ldap/__init__.py
@@ -6,7 +6,7 @@ LDAP authentification for udata with optionnal Kerberos suppport.
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.2.0.dev'
+__version__ = '0.2.0'
 __description__ = 'LDAP authentification for udata with optional Kerberos suppport.'
 
 
